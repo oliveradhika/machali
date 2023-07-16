9aed43d3ebf562eebfe1e0d95bd430ad04783b45
oliveradhika <shreeram@puplease.com>
2023-07-16 07:27:08 +0530

    initial

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: false,
+// };
+
+// module.exports = nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+}
+ 
+module.exports = nextConfig
